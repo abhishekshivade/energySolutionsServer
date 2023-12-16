bdbd65573a5bede9dc7427dc526403f0a16bbb09
abhishekshivade <abhishekshivade@gmail.com>
2023-12-16 14:13:41 +0530

    email unique

--- userSchema.js
+++ userSchema.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
     userName: {type: String, require: true},
     address:  {type: String, require: true},
     mobileNumber:  {type: String, require: true},
-    email:  {type: String, require: true},
+    email:  {type: String, require: true, unique: true},
     password:  {type: String, require: true},
     // salt:  {type: String, require: true},
     companies: {type: [String], require: true}
